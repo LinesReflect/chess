a728c47af92997bf4ffbc612e3a80c2132d3ebbc
Reflection <reflectionoflines@gmail.com>
2023-05-30 12:41:58 -0500

    Create the initializer method.

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
+require_relative 'board'
+require_relative 'player'
+require 'colorize'
+
 class Game
+  attr_reader :board
+
+  def initialize(player_1 = Player.new, player_2 = Player.new, board = Board.new)
+    @player_1 = player_1
+    @player_2 = player_2
+    @board = board
+  end
 end
