626a0e62ce039c0edd673a6146e673a4ebd4fe34
Reflection <reflectionoflines@gmail.com>
2023-06-01 12:53:39 -0500

    Add methods that set player names and piece colors.

--- lib/game.rb
+++ lib/game.rb
@@ -7,9 +7,31 @@ require 'colorize'
 class Game
   attr_reader :board
 
-  def initialize(player_1 = Player.new, player_2 = Player.new, board = Board.new)
-    @player_1 = player_1
-    @player_2 = player_2
+  def initialize(player1 = Player.new(1), player2 = Player.new(2), board = Board.new)
+    @player1 = player1
+    @player2 = player2
     @board = board
+    @player_turn = nil
+  end
+
+  def game_setup
+    game_greeting
+    player_names
+    player_colors
+    puts "#{@player1.name} will control #{@player1.color} pieces and #{@player2.name} controls the #{@player2.color} pieces."
+  end
+
+  def game_greeting
+    puts "Hello players, let's set up the game so we can start!"
+  end
+
+  def player_names
+    @player1.player_name
+    @player2.player_name(@player1.name)
+  end
+
+  def player_colors
+    @player1.choose_player_color
+    @player2.choose_player_color(@player1.color)
   end
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,4 +1,40 @@
 # frozen_string_literal: true
 
 class Player
+  attr_reader :number, :name, :color
+
+  def initialize(number)
+    @number = number
+    @name = nil
+    @color = nil
+  end
+
+  def player_name(opponent_name = nil)
+    loop do
+      puts player_greeting
+      input = gets.chomp
+      return @name = input if valid_name?(input, opponent_name)
+    end
+  end
+
+  def player_greeting()
+    "Player #{@number}, please enter a unique name at least 3 characters long!"
+  end
+
+  def valid_name?(name, opponent_name = nil)
+    return false if name.length < 3 || name == opponent_name
+
+    true
+  end
+
+  def choose_player_color(opponent_color = nil)
+    return @color = opponent_color == 'white' ? 'black' : 'white' unless opponent_color.nil?
+
+    loop do 
+      puts "#{@name}, press 1 to use white pieces or 2 for black pieces."
+      input = gets.chomp.to_i
+      redo unless [1, 2].include?(input)
+      return @color = input == 1 ? 'white' : 'black'
+    end
+  end
 end
