b859f82f22f5b0446695bf0ccb0d2785660d6027
Reflection <reflectionoflines@gmail.com>
2023-08-02 12:48:10 -0500

    Let a piece add to or clear an array of possible moves.

--- lib/piece.rb
+++ lib/piece.rb
@@ -3,18 +3,42 @@
 require_relative 'board'
 require_relative 'player'
 require_relative 'square'
+require_relative 'movesets'
 
 class Piece
-  attr_reader :player, :color, :current_square, :symbol
+  attr_reader :player, :color, :current_square, :fen, :symbol, :moves
 
-  def initialize(player, color, current_square, symbol = colored_symbol(color))
+  include Movesets
+
+  def initialize(player, color, current_square, fen, symbol = colored_symbol(color))
     @player = player
     @color = color
     @current_square = current_square
+    @fen = fen
     @symbol = symbol
+    @moves = []
+  end
+
+  def change_square(square)
+    @current_square.no_piece
+    @current_square = square
+    claim_square
   end
 
   def claim_square
     current_square.update_piece(self)
+  end
+
+  def clear_moves
+    @moves = []
+  end
+
+  def print_moves
+    i = 0
+    while i < @moves.length
+      print "#{i + 1}.#{moves[i].rank_and_file}"
+      i == @moves.length - 1 ? print("\n") : print(' ')
+      i += 1
+    end
   end
 end
